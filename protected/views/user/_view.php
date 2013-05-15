1565cd95a49859a585f2755108a6e42b088cc0ca
Hisateru Tanaka <tanakahisateru@gmail.com>
2013-05-16 02:36:22 +0900

    Create user crud base

---
+++ protected/views/user/_view.php
@@ -0,0 +1,16 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id),array('view','id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('fullName')); ?>:</b>
+	<?php echo CHtml::encode($data->fullName); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('twitterName')); ?>:</b>
+	<?php echo CHtml::encode($data->twitterName); ?>
+	<br />
+
+
+</div>
