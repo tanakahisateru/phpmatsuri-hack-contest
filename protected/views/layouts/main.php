22e527ba9cf206f80276c74d8c86a80cb7dcffd5
Hisateru Tanaka <tanakahisateru@gmail.com>
2013-05-18 03:49:29 +0900

    Fixed viewport

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
