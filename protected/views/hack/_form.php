511b6506f948fb5edce502fde50702ff716c3c02
Hisateru Tanaka <tanakahisateru@gmail.com>
2013-06-20 21:15:26 +0900

    Link to PHP Markdown reference. #9

--- protected/views/hack/_form.php
+++ protected/views/hack/_form.php
@@ -13,8 +13,18 @@
 
 	<?php echo $form->textFieldRow($model,'title',array('class'=>'span5','maxlength'=>255)); ?>
 
-	<?php echo $form->textAreaRow($model,'description',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
-
+	<?php echo $form->textAreaRow($model,'description',array('rows'=>20, 'cols'=>50, 'class'=>'span8')); ?>
+	<div class="hint">
+		<?php $this->widget('bootstrap.widgets.TbButton', array(
+			'type'=>'link',
+			'icon'=>'book',
+			'label'=>'PHP Markdown Reference',
+			'url'=>'http://michelf.ca/projects/php-markdown/reference/',
+			'htmlOptions'=>array(
+				'target'=>'_blank',
+			)
+		)); ?>
+	</div>
 
 	<div class="form-actions">
 		<?php $saveLabel = (isset($_GET['from']) && $_GET['from'] == 'reviewPage') ? 'Save and back to the review page' : 'Save'; ?>
