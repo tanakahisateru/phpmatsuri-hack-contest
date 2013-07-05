a1fac8b1f70ba71c7f57be07e6e692dbd987d412
Hisateru Tanaka <tanakahisateru@gmail.com>
2013-07-05 12:14:43 +0900

    Config Logger

--- protected/config/main.php
+++ protected/config/main.php
@@ -72,12 +72,10 @@ return array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
 				),
-				// uncomment the following to show log messages on web pages
-				/*
 				array(
 					'class'=>'CWebLogRoute',
+					'enabled'=>false,
 				),
-				*/
 			),
 		),
 		'widgetFactory'=>array(
